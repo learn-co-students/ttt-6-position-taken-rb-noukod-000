42b56931c9b446ddf7564907e1a5fe7e0f4e0d7d
Mac Dowell Prosper <macdowell@noukod.com>
2018-08-30 19:20:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(array,index)
+  if (array[index] == " " or array[index] == "" or array[index] == nil)
+    return false
+  else
+    return true
+  end
+end
