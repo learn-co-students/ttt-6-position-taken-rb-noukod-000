61ee2ba80ef188e69354e2808e73840c64c3c56e
Keren-Happuc Fleurimond <keren@noukod.com>
2018-08-31 17:52:20 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else board[index] == " " || [index] == "" || board[index] == nil
+    return false
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0, 10)
+    return true
+  else
+    return false
+  end
+end
